4e872710e6d75b1c7fe5b785d15ed4306a120e2f
ZohebS-dxb <zubisid@gmail.com>
2025-06-19 03:21:17 +0400

    Update buttons with mobile animations and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+        sans: ['Poppins', 'sans-serif'],
       },
     },
   },
-  variants: {},
   plugins: [],
 };
